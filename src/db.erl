8ee67651714da6f85b203c1fdf776e5285f27bec
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2021-01-17 17:56:34 +0100

    Refactor

---
+++ src/db.erl
@@ -0,0 +1,61 @@
+-module(db).
+-import(util, [map_to_json/1]).
+-export([find_by_id/2, insert/3, update/3, delete/2, generate_id/1, db/2]).
+
+find_by_id(Collection, RecordId) -> 
+	Offers = db(Collection, fun() -> dets:lookup(records_db, RecordId) end),
+	case Offers of
+		[{RecordId2, Data}] ->
+			{200, map_to_json({RecordId2, Data})};
+		[] ->
+			{404, io_lib:format("{\"not_found\": \"record ~s not found\"}", [RecordId])};
+		_ ->
+			{500, io_lib:format("{\"extra_records\": \"extra records for ~s\"}", [RecordId])}
+	end.
+
+insert(Collection, Id, Record) -> 
+	ok = db(Collection, fun () -> 
+    	ok = dets:insert(records_db, {Id, Record}),
+    	dets:sync(records_db)
+	end),
+	{200, Inserted} = find_by_id(Collection, Id),
+	Inserted.
+
+update(Collection, Id, Offer) ->
+	{200, _} = find_by_id(Collection, Id),
+	delete(Collection, Id),
+	insert(Collection, Id, Offer).
+
+delete(Collection, RecordId) ->
+	ok = db(Collection, fun () -> dets:delete(records_db, RecordId) end).
+
+
+generate_id(Collection) ->
+	Filename = case Collection of
+		offers -> offers_state_file_name;
+		orders -> orders_state_file_name
+	end,
+	{ok, Statefilename} = application:get_env(pw, Filename),
+    dets:open_file(state_db, [{file, Statefilename}, {type, set}]),
+    Records = dets:lookup(state_db, current_id),
+    Response = case Records of
+        [{current_id, CurrentId}] ->
+            NextId = CurrentId + 1,
+            dets:insert(state_db, {current_id, NextId}),
+            lists:flatten(io_lib:format("id_~4..0B", [CurrentId]));
+        [] ->
+            error
+    end,
+    dets:close(state_db),
+    Response.
+
+db(Collection, Action) ->
+	Filename = case Collection of
+		offers -> offers_records_file_name;
+		orders -> orders_records_file_name
+	end,
+    {ok, Recordfilename} = application:get_env(pw, Filename),
+    {ok, _} = dets:open_file(records_db, [{file, Recordfilename}, {type, set}]),
+    Result = Action(),
+    ok = dets:close(records_db),
+	Result.
--- src/orders_handler.erl
+++ src/orders_handler.erl
@@ -1,5 +1,6 @@
 -module(orders_handler).
--import(util, [response/3, map_to_json/1, generate_id/1, db/2]).
+-import(util, [response/3, map_to_json/1]).
+-import(db, [find_by_id/2, insert/3, update/3, delete/2, generate_id/1, db/2]).
 -behavior(cowboy_handler).
 
 -export([init/2]).
@@ -8,7 +9,7 @@ init(Req=#{method := <<"GET">>}, State) ->
 	Params = cowboy_req:parse_qs(Req),
 	{Status, Values} = case Params of 
 		[{<<"id">>, OrderId}] ->
-			find_order_by_id(binary_to_list(OrderId));
+			find_by_id(orders, binary_to_list(OrderId));
 		[] -> 
 			F = fun (Order, Acc) -> Acc1 = [map_to_json(Order) | Acc], Acc1 end,
 			Items = db(orders, fun() -> dets:foldl(F, [], records_db) end),
@@ -23,51 +24,40 @@ init(Req=#{method := <<"GET">>}, State) ->
     {ok, Res, State};
 
 init(Req=#{method := <<"POST">>}, State) ->
-	RecordId = generate_id(orders),
-    {ok, [
-		{<<"offerId">>, OfferId},
-		{<<"quantity">>, Quantity},
-		{<<"amount">>, Amount},
-		{<<"buyer">>, Buyer}
-	], _ } = cowboy_req:read_urlencoded_body(Req),
-	Order = #{
-			id => {RecordId, text},
-			offer_id => {OfferId, text},
-			quantity => {Quantity, float},
-			amount => {Amount, float},
-			buyer => {Buyer, text}
-		},
-	ok = db(orders, fun () -> 
-    	ok = dets:insert(records_db, {RecordId, Order}),
-    	dets:sync(records_db)
-	end),
-	{200, Inserted} = find_order_by_id(RecordId),
+	OrderId = generate_id(orders),
+	Order = order_from_body(OrderId, Req),
+	Inserted = insert(orders, OrderId, Order),
 	Res = response(201, Inserted, Req),
     {ok, Res, State};
 
 init(Req=#{method := <<"PUT">>}, State) ->
-    Res = cowboy_req:reply(200, #{
-        <<"content-type">> => <<"text/plain">>
-    }, <<"Hello world!">>, Req),
+	[{<<"id">>, OrderIdRaw}] = cowboy_req:parse_qs(Req),
+	OrderId = binary_to_list(OrderIdRaw),
+	Order = order_from_body(OrderId, Req),
+	Updated = update(orders, OrderId, Order),
+    Res = response(200, Updated, Req),
     {ok, Res, State};
 
 init(Req=#{method := <<"DELETE">>}, State) ->
 	[{<<"id">>, OrderId}] = cowboy_req:parse_qs(Req),
-    RecordId1 = binary_to_list(OrderId),
-	ok = db(orders, fun () -> dets:delete(records_db, RecordId1) end),
+	delete(orders, binary_to_list(OrderId)),
     {ok, response(204, <<"">>, Req), State};
 
 init(Req, State) ->
     Res = cowboy_req:reply(405, #{}, Req),
     {ok, Res, State}.
 
-find_order_by_id(OrderId) -> 
-	Orders = db(orders, fun() -> dets:lookup(records_db, OrderId) end),
-	case Orders of
-		[{OrderId2, Data}] ->
-			{200, map_to_json({OrderId2, Data})};
-		[] ->
-			{404, io_lib:format("{\"not_found\": \"record ~s not found\"}", [OrderId])};
-		_ ->
-			{500, io_lib:format("{\"extra_records\": \"extra records for ~s\"}", [OrderId])}
-	end.
+order_from_body(Id, Req) -> 
+	{ok, [
+		{<<"offerId">>, OfferId},
+		{<<"quantity">>, Quantity},
+		{<<"amount">>, Amount},
+		{<<"buyer">>, Buyer}
+	], _ } = cowboy_req:read_urlencoded_body(Req),
+	#{
+		id => {Id, text},
+		offer_id => {OfferId, text},
+		quantity => {Quantity, float},
+		amount => {Amount, float},
+		buyer => {Buyer, text}
+	}.
